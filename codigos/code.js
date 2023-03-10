62e1ed1e34bc4f9c914a87bd5d956def2cef7dbf
Amaral-Ethore <ethorea.m@gmail.com>
2023-03-10 01:38:20 -0300

    fixes

--- codigos/code.js
+++ codigos/code.js
@@ -1,9 +1,5 @@
-const myDropdown = document.getElementById('myDropdown')
-myDropdown.addEventListener('show.bs.dropdown', event => {
-  console.log(event);
-})
 document.getElementById('dropbtn').addEventListener('click',() =>{
         let dropdown = document.getElementById('myDropdown');
-        dropdown.style.transform = "translate(-145px, 22px)";
+        dropdown.style.transform = "translate(-145px, 40px)";
         console.log(dropdown);    
-})
+});
