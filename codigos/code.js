a7bbdac6402af69d9ef808c98836c5906ca1d69b
bsn <bernardoschossler54@gmail.com>
2023-04-04 21:21:25 -0300

    teste de novo

--- codigos/code.js
+++ codigos/code.js
@@ -2,4 +2,4 @@ document.getElementById('dropbtn').addEventListener('click',() =>{
         let dropdown = document.getElementById('myDropdown');
         dropdown.style.transform = "translate(-145px, 40px)";
         console.log(dropdown);    
-});
+}); 
---
+++ public/nav.php
@@ -0,0 +1,86 @@
+<header>
+            <div class="h-1">
+                <div class="logo">
+                    <a href="index.php">
+                        <img class="logoimg" src="../imagens/aber-logo-zip-file/png/logo-no-background.png" alt="logo">
+                    </a>
+                </div>
+                <div class="menu">
+                    <ul>
+                        <li>
+                            <div class="search">
+                                <input id="search" class="menu-input" type="text">
+                                <i class=" lupa fa-solid fa-magnifying-glass"></i>
+                            </div>
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-plus"></i>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fa-solid fa-cart-shopping"></i>
+                            </a>
+                        </li>
+                        <li class="hover">
+                            <div class="dropdown">
+                                <i id="dropbtn" class="fa-solid fa-user" data-bs-toggle="dropdown" aria-expanded="false"
+                                    data-bs-auto-close="outside"></i>
+                                </button>
+                                <form id="myDropdown" class="dropdown-menu p-4 login-form">
+                                    <div class="mb-3">
+                                        <label for="exampleDropdownFormEmail2" class="form-label">Email </label>
+                                        <input type="email" class="form-control" id="exampleDropdownFormEmail2"
+                                            placeholder="email@examplo.com">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="exampleDropdownFormPassword2" class="form-label">Senha</label>
+                                        <input type="password" class="form-control" id="exampleDropdownFormPassword2"
+                                            placeholder="Senha">
+                                    </div>
+                                    <div class="mb-3">
+                                        <div class="form-check">
+                                            <input type="checkbox" class="form-check-input" id="dropdownCheck2">
+                                            <label class="form-check-label" for="dropdownCheck2">
+                                                Lembrar Senha
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Logar</button>
+                                </form>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="h-2">
+                <div class="menu-2">
+                    <ul>
+                        <li>
+                            <a href="item.php">
+                                Ofertas
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Moda
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Mais Acessados
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Vender
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Contato
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
