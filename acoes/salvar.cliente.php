6eb5ccc505c6728275ed3119928a5670c550d39f
ADONIASP <adonias.pires76@gmail.com>
2023-04-10 19:02:25 -0300

    acoes

---
+++ acoes/salvar.cliente.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+
+require_once(str_replace('\\', '/', dirname(__FILE__, 2)) . '/acoes/verifica_sessao.php');
+require_once(str_replace('\\', '/', dirname(__FILE__, 2)) . "/classes/cliente.class.php");
+require_once(str_replace('\\', '/', dirname(__FILE__, 2)) . "/controllers/cliente.controller.php");
+
+$cliente = new Cliente();
+
+if (isset($_POST) && isset($_POST['id']) && !empty($_POST['id'])) {
+    $id         = intval(addslashes(filter_input(INPUT_POST, 'id')));
+    $nome       = addslashes(filter_input(INPUT_POST, 'nome'));
+    $endereco   = addslashes(filter_input(INPUT_POST, '$endereco'));
+    $senha       = addslashes(filter_input(INPUT_POST, 'senha'));
+    
+    $cpfcnpj    = addslashes(filter_input(INPUT_POST, 'cpf_cnpj'));
+    $telefone   = addslashes(filter_input(INPUT_POST, 'telefone'));
+    var_dump($cpfcnpj);
+
+    if (empty($nome) || empty($cpfcnpj)) {
+        $_SESSION['mensagem'] = "Obrigatório informar Nome e CPF/CNPJ 2";
+        $_SESSION['sucesso'] = false;
+        header('Location:../public/cad_cliente.php?key=' . $id);
+        die();
+    }
+    $cliente->setId($id);
+    $cliente->setNome(($nome));
+    $cliente->setEndereco(($endereco));
+    $cliente->setSenha(($senha));
+    $cliente->setEmail(($email));
+    $cliente->setCpfCnpj($cpf_cnpj);
+    $cliente->setTelefone($telefone);
+    $controller = new ClienteController();
+    $resultado = $controller->atualizarCliente($cliente);
+
+    if ($resultado) {
+        $_SESSION['mensagem'] = "Atualizado com sucesso";
+        $_SESSION['sucesso'] = true;
+    } else {
+        $_SESSION['mensagem'] = "Erro ao atualizar";
+        $_SESSION['sucesso'] = false;
+    }
+    header('Location:../public/cad_cliente.php');
+} else {
+
+    $nome = isset($_POST['nome']) ? $_POST['nome'] : null;
+    $cpfcnpj = isset($_POST['cpf_cnpj']) ? $_POST['cpf_cnpj'] : null;
+    $telefone = isset($_POST['telefone']) ? $_POST['telefone'] : null;
+
+    if ($nome && $cpfcnpj) {
+
+        $cliente->setId($id);
+        $cliente->setNome(($nome));
+        $cliente->setEndereco(($endereco));
+        $cliente->setSenha(($senha));
+        $cliente->setEmail(($email));
+        $cliente->setCpfCnpj($cpf_cnpj);
+        $cliente->setTelefone($telefone);
+
+        $dao = new ClienteController();
+        $resultado = $dao->criarCliente($cliente);
+        if ($resultado) {
+            $_SESSION['mensagem'] = "Criado com sucesso";
+            $_SESSION['sucesso'] = true;
+        } else {
+            $_SESSION['mensagem'] = "Erro ao criar";
+            $_SESSION['sucesso'] = false;
+        }
+    } else {
+        $_SESSION['mensagem'] = "Obrigatório informar Nome e CPF/CNPJ";
+        $_SESSION['sucesso'] = false;
+    }
+    header('Location:../public/cad_cliente.php');
+}
