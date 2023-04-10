7626bc8e5239781cbe05ca81245b26b87ce704f4
Amaral-Ethore <ethorea.m@gmail.com>
2023-04-10 18:01:47 -0300

    sla

--- controller/login.funcionario.php
+++ controller/login.funcionario.php
@@ -1,13 +1,18 @@
 <?php
 require_once('../classes/funcionario.class.php');
 require_once('../controller/funcionario.controller.php');
-function buscaFuncionario($mailfuncionario, $senhafuncionario){
+function buscaFuncionario($mail, $senha){
     $controller = new FuncionarioController();
     $funcionario = new Funcionario();
-    $funcionario = $controller->buscarPorEmail($mailfuncionario);
-    if($mailfuncionario && $senhafuncionario == $funcionario->getSenha()){
-        $_SESSION['tipo'] = "funcionario";
-        $_SESSION['usuario'] = $funcionario->getEmail();
-        echo($_SESSION['usuario']);
+    $funcionario = $controller->buscarPorEmail($mail);
+    if(isset($funcionario)){
+        if($senha == $funcionario->getSenha()){
+            $_SESSION['tipo'] = "funcionario";
+            $_SESSION['usuario'] = $funcionario->getEmail();
+            echo($_SESSION['usuario']);
+        }
+        else{
+            $_SESSION['usuario'] = "invalido";
+        }
     }
 }
