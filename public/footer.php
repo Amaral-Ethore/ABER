f82b8e3e221d16425da8855e7be56a37f5e4d5b7
bsn <bernardoschossler54@gmail.com>
2023-04-11 21:23:25 -0300

    footer com espaço

--- public/footer.php
+++ public/footer.php
@@ -1,3 +1,12 @@
+<footer>
+    <div>a</div>
+    <div>b</div>
+    <div>c</div>
+    <div>d</div>
+    <div>e</div>
+</footer>
+
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 </body>
 </html>
--- public/item.php
+++ public/item.php
@@ -111,11 +111,11 @@
                 </div>
             </div>
         </main>
-
+        <?php require_once('footer.php')?>
 
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"></script>
+    
 </body>
 
 </html>
