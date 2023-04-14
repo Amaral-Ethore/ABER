e9e982e9bced630dde40ea3c8cfd4534e2e99bc2
bsn <bernardoschossler54@gmail.com>
2023-04-14 18:39:06 -0300

    ok

--- public/item.php
+++ public/item.php
@@ -100,7 +100,8 @@
                     </h2>
                     <div id="panelsStayOpen-collapseOne" class="accordion-collapse bs-warning-bg-subtle collapse show">
                         <div class="accordion-body">
-                            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            <strong>Isso aqui ainda aceita formatação do HTML.</strong>
+                            <p>Tipo parágrafos, <span>spans,</span> e todo o resto.</p>
                         </div>
                     </div>
                 </div>
