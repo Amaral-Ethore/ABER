8bad512e6c51ad19b4da9138723b88606bd80f5f
bsn <bernardoschossler54@gmail.com>
2023-04-26 19:43:03 -0300

    ok

--- acoes/carrinho.add.php
+++ acoes/carrinho.add.php
@@ -1,14 +1,17 @@
 <?php 
-require_once(str_replace('\\', '/', dirname(__FILE__, 2)) . "../classes/compra.class.php");
-require_once(str_replace('\\', '/', dirname(__FILE__, 2)) . "../controller/carrinho.controller.php");
+require_once(str_replace('\\', '/', dirname(__FILE__, 2)) . "/classes/compra.php");
+require_once(str_replace('\\', '/', dirname(__FILE__, 2)) . "/controller/carrinho.controller.php");
 
 $compra = new Carrinho();
 
-if (isset($_GET) && isset($_GET['id']) && !empty($_GET['id'])) {
-    $id = intval(addslashes(filter_input(INPUT_GET, 'id')));
+if (isset($_GET) && isset($_GET['key'])) {
+    $id = intval(addslashes(filter_input(INPUT_GET, 'key')));
     $controller = new CarrinhoController();
-    $controller-> buscarNome($id);
+    $controller-> buscarPorId($id);
 
+    header("Location:../public/index.php");
     
-    
+}
+else {
+    header("Location:../public/item.php");
 }
